c0499e8b03126e82798ed01a5415412279c1d5cd
Haseeb-ur-rehman <forsocialsss.3@gmail.com>
2026-03-20 14:19:14 +0500

    fix type error

--- weather-app/contexts/AppContext.tsx
+++ weather-app/contexts/AppContext.tsx
@@ -1,9 +1,13 @@
 "use client"
-import { createContext, useState } from "react";
-export const appContext = createContext(null);
-export default function DataContext({ children }) {
+import { createContext, Dispatch, SetStateAction, useState } from "react";
+interface AppContext {
+  value:string;
+  setValue:Dispatch<SetStateAction<string>>
+}
+export const appContext = createContext<AppContext | null>(null);
+export default function DataContext({ children }: Readonly<{ children: React.ReactNode }>) {
 
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState<string>('')
   return <appContext.Provider value={{ value, setValue }}>
     {children}
   </appContext.Provider>;
